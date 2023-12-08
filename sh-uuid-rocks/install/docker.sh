244d19a24431cf2ad409bad8385169f6e6e7f15d
Jacob Hands <jacob@gogit.io>
2023-12-08 06:56:04 -0600

    Confirm install

--- sh-uuid-rocks/install/docker.sh
+++ sh-uuid-rocks/install/docker.sh
@@ -22,4 +22,4 @@ echo \
 sudo apt-get update
 
 # Install latest version:
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
